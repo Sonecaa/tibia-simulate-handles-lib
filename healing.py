495181780c1bac21080e43381ff8d8ed19e7b1e0
Marcelo Assis <marcelo.assis.santos@live.com>
2019-08-28 12:18:58 -0300

    fixes

--- healing.py
+++ healing.py
@@ -23,32 +23,28 @@ class Healing:
     return pyautogui.locateOnScreen("images/follow.png")
 
   def get_mana(self):
-    return pyautogui.locateOnScreen("images/mana.png")
 
   def get_health(self):
-    return pyautogui.locateOnScreen("images/health.png")
+    health = pyautogui.locateOnScreen("images/health.png")
+    if (pyautogui.pixelMatchesColor((health.left + 105), (health.top + 7), (219, 79, 79))):
+        return 100
+    elif (pyautogui.pixelMatchesColor((health.left + 94), (health.top + 7), (219, 79, 79))):
+        return 90
+    elif (pyautogui.pixelMatchesColor((health.left + 74), (health.top + 7), (219, 79, 79))):
+        return 70
+    elif (pyautogui.pixelMatchesColor((health.left + 54), (health.top + 7), (219, 79, 79))):
+        return 50
+    elif (pyautogui.pixelMatchesColor((health.left + 34), (health.top + 7), (219, 79, 79))):
+        return 30
+    elif (pyautogui.pixelMatchesColor((health.left + 24), (health.top + 7), (219, 79, 79))):
+        return 20
+    elif (pyautogui.pixelMatchesColor((health.left + 14), (health.top + 7), (219, 79, 79))):
+        return 15
+    elif (pyautogui.pixelMatchesColor((health.left + 5), (health.top + 7), (219, 79, 79))):
+        return 10
+    return 0
 
-  def getLife(self):
-  health = pyautogui.locateOnScreen("images/health.png")
-  if (pyautogui.pixelMatchesColor((health.left + 105), (health.top + 7), (219, 79, 79))):
-      return 100
-  elif (pyautogui.pixelMatchesColor((health.left + 94), (health.top + 7), (219, 79, 79))):
-      return 90
-  elif (pyautogui.pixelMatchesColor((health.left + 74), (health.top + 7), (219, 79, 79))):
-      return 70
-  elif (pyautogui.pixelMatchesColor((health.left + 54), (health.top + 7), (219, 79, 79))):
-      return 50
-  elif (pyautogui.pixelMatchesColor((health.left + 34), (health.top + 7), (219, 79, 79))):
-      return 30
-  elif (pyautogui.pixelMatchesColor((health.left + 24), (health.top + 7), (219, 79, 79))):
-      return 20
-  elif (pyautogui.pixelMatchesColor((health.left + 14), (health.top + 7), (219, 79, 79))):
-      return 15
-  elif (pyautogui.pixelMatchesColor((health.left + 5), (health.top + 7), (219, 79, 79))):
-      return 10
-  return 0
-
-  def getMana(self):
+  def get_mana(self):
       health = pyautogui.locateOnScreen("images/mana.png")
       if (pyautogui.pixelMatchesColor((health.left + 105), (health.top + 5), (67, 64, 192))):
           return 100
@@ -68,16 +64,16 @@ class Healing:
           return 10
       return 0
 
-  def heal(self,percent, spell,time):
+  def heal_health(self, percent, spell, time):
       life = self.getLife(self)
       while life < percent:
-          #w.Write(spell) ***
-          sleep(time)
+          keyboard.write(spell)
+          time.sleep(time)
           life = self.getLife(self)
 
-  def mp(self,percent, spell,time):
+  def heal_mana(self, percent, spell, time):
       mana = self.getMana(self)
       while mana > percent:
-          #w.Write(spell) ***
-          sleep(time)
+          keyboard.write(spell)
+          time.sleep(time)
           mana = self.getMana(self)
